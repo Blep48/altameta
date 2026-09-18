3bc6d44b0691ccde4c082cb8f3d050037ad21a45
Blep48 <42011357+Blep48@users.noreply.github.com>
2026-09-18 05:46:35 +0200

    Submit Monkey friend score

--- src/routes/play.memory.tsx
+++ src/routes/play.memory.tsx
@@ -1,3 +1,4 @@
+import { submitIfFriend } from "@/lib/duel/friend-match";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useDuel } from "@/lib/duel/provider";
